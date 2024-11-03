126ce5e1ddf0e4581a343ce407e912a5aa10a0db
ccaglak <ccaglak@gmail.com>
2024-11-03 22:29:49 +0300

    update

--- lua/larago/go.lua
+++ lua/larago/go.lua
@@ -4,52 +4,32 @@ local pop = require("larago.ui")
 local trs = require("larago.treesitter")
 local utils = require("larago.utils")
 
-local sep = vim.uv.os_uname().sysname == "Windows_NT" and "\\" or "/"
-local M = {}
+-- Cache system variables
+local cache = {
+  root_dir = nil,
+  tags = {},
+  sep = vim.uv.os_uname().sysname == "Windows_NT" and "\\" or "/"
+}
 
--- Cache root directory
-local root_dir = nil
+-- Patterns compiled once
+local PATTERNS = {
+  include = [=[include%(['"]([^'"]+)['"])]=],
+  livewire = [=[livewire%(['"]([^'"]+)['"])]=],
+  component = [=[component%(['"]([^'"]+)['"])]=]
+}
+
 local function get_root_dir()
-  if not root_dir then
-    root_dir = vim.fs.root(0, { ".git", "composer.json" }) or vim.uv.cwd()
+  if not cache.root_dir then
+    cache.root_dir = vim.fs.root(0, { ".git", "composer.json" }) or vim.uv.cwd()
   end
-  return root_dir
+  return cache.root_dir
 end
 
--- Pre-compile patterns
-local INCLUDE_PATTERN = [[include%('([^']+)]]
-local LIVEWIRE_PATTERN = [[livewire%('([^']+)]]
-
--- Buffer-local tag cache
-local tag_cache = {}
 local function check_tag(tag_name)
-  if tag_cache[tag_name] == nil then
-    tag_cache[tag_name] = tags:contains(tag_name)
+  if cache.tags[tag_name] == nil then
+    cache.tags[tag_name] = tags:contains(tag_name)
   end
-  return tag_cache[tag_name]
-end
-
-
-
-vim.api.nvim_create_autocmd("BufWritePre", {
-  callback = function(event)
-    if event.match:match('^%w%w+://') then return end
-    local file = vim.uv.fs_realpath(event.match) or event.match
-    vim.fn.mkdir(vim.fn.fnamemodify(file, ':p:h'), 'p')
-  end,
-})
-
-M.parsed_dir = function(file)
-  local rd = get_root_dir()
-  local path = rd .. sep .. "resources" .. sep .. "views" .. sep
-  if #file == 1 then return path end
-
-  for i, value in pairs(file) do
-    if i < #file then
-      path = path .. value .. sep
-    end
-  end
-  return path
+  return cache.tags[tag_name]
 end
 
 local function rg_search(pattern, path)
@@ -60,32 +40,108 @@ local function rg_search(pattern, path)
     "--files",
     path
   }):wait()
-
   return vim.split(output.stdout, "\n")
 end
 
--- Update rgSearch to handle the new output format
-M.rgSearch = function(path, file)
-  local result = rg_search(file, path)
-  return result[1] -- Return first match
+local M = {}
+
+M.parsed_dir = function(file)
+  local path = get_root_dir() .. cache.sep .. "resources" .. cache.sep .. "views" .. cache.sep
+  if #file == 1 then return path end
+
+  for i, value in ipairs(file) do
+    if i < #file then
+      path = path .. value .. cache.sep
+    end
+  end
+  return path
 end
 
--- Update rgcSearch to use the new search function
+M.rgSearch = function(path, file)
+  return (rg_search(file, path))[1]
+end
+
 M.rgcSearch = function(file)
-  local rd = get_root_dir()
-  return rg_search(file, rd .. sep .. "resources")
+  return rg_search(file, get_root_dir() .. cache.sep .. "resources")
+end
+
+M._toBlade = function(txt)
+  local split = utils.spliter(txt)
+  local path = M.parsed_dir(split)
+  local bladeFile = M.rgSearch(path, split[#split])
+
+  local target = bladeFile or (path .. split[#split] .. ".blade.php")
+  vim.cmd("e " .. vim.fn.fnameescape(target))
+end
+
+M.search = function(search)
+  local results = M.rgcSearch(search)
+  if #results > 1 then
+    vim.schedule(function() pop.popup(results) end)
+    return
+  end
+  vim.cmd("e " .. vim.fn.fnameescape(results[1]))
 end
 
 local node_handlers = {
-  function_call_expression = function(node) M.view(node) end,
-  member_call_expression = function(node) M.route_name(node) end,
-  self_closing_tag = function(node) M.tag(node) end,
-  tag_name = function(node) M.tag(node) end,
-  text = function() M.include() end,
-  nowdoc_string = function(node) M.nowdoc(node) end,
-  start_tag = function(node)
-    local _, name_node = trs.children(node, "tag_name")
-    M.tag(name_node)
+  function_call_expression = function(node)
+    if vim.bo.filetype ~= "php" then return end
+    local fn = trs.get_name(trs.child(node, "function"))
+    if fn == "view" then
+      local val = trs.children(trs.child(node, "arguments"), "argument")
+      if val then M._toBlade(val:gsub("'", "")) end
+    end
+  end,
+
+  member_call_expression = function(node)
+    if vim.bo.filetype ~= "php" then return end
+    local fn = trs.get_name(trs.child(node, "name"))
+    if fn == "name" then
+      local val = trs.children(trs.child(node, "arguments"), "argument")
+      if val then M._toBlade(val:gsub("'", "")) end
+    end
+  end,
+
+  tag = function(node)
+    if vim.bo.filetype ~= "html" then return end
+    local cmp = ts.get_node_text(node, 0, {})
+    if not cmp then return end
+    if check_tag(cmp) then
+      vim.notify_once("Native HTML Tag")
+      return
+    end
+
+    local nt = node:next_sibling()
+    local att = trs.get_name(nt)
+
+    if cmp:find(":", 1, true) then
+      local scmp = utils.spliter(cmp, ":")
+      if att:find(".", 1, true) then
+        M.search(utils.spliter(att, ".")[#utils.spliter(att, ".")])
+        return
+      end
+      M.search(scmp[#scmp])
+      return
+    end
+
+    local split = utils.spliter(cmp, "-")
+    local search = split[#split]
+    if search == "layouts" or search == "layout" then
+      search = split[#split - 1]
+    end
+    if #split > 3 then
+      search = split[#split - 1] .. "-" .. split[#split]
+    end
+
+    local results = M.rgcSearch(search)
+    if #results > 1 then
+      vim.schedule(function() pop.popup(results) end)
+      return
+    elseif #results == 0 then
+      M.search(split[#split - 1] .. "-" .. split[#split])
+      return
+    end
+    vim.cmd("e " .. vim.fn.fnameescape(results[1]))
   end
 }
 
@@ -93,16 +149,16 @@ M.to = function()
   if not vim.bo.filetype then return end
   utils.filetype()
 
-  for _, value in pairs({
+  for _, node_type in ipairs({
     "function_call_expression",
     "member_call_expression",
     "tag_name",
     "self_closing_tag",
     "text",
     "nowdoc_string",
-    "start_tag",
+    "start_tag"
   }) do
-    local node = trs.parent(value)
+    local node = trs.parent(node_type)
     if node then
       local handler = node_handlers[node:type()]
       if handler then
@@ -113,122 +169,25 @@ M.to = function()
   end
 end
 
-M.nowdoc = function(node)
-  local line = trs.get_name(node)
-  if not line then return end
-  M.include(line)
-end
-
-M._toBlade = function(txt)
-  local split = utils.spliter(txt)
-  local path = M.parsed_dir(split)
-  local bladeFile = M.rgSearch(path, split[#split])
-
-  if bladeFile then
-    vim.cmd("e " .. vim.fn.fnameescape(bladeFile))
-    return
-  end
-  vim.cmd("e " .. vim.fn.fnameescape(path .. split[#split] .. ".blade.php"))
-end
-
-M.search = function(search)
-  local rc = M.rgcSearch(search)
-  if #rc > 1 then
-    vim.schedule(function()
-      pop.popup(rc)
-    end)
-    return
-  end
-  vim.cmd("e " .. vim.fn.fnameescape(unpack(rc)))
-end
-
 M.include = function(line)
   line = line or vim.api.nvim_get_current_line()
-  local txt = string.match(line, INCLUDE_PATTERN)
-  if not txt then
-    txt = string.match(line, LIVEWIRE_PATTERN)
-    if not txt then return end
-    local split = utils.spliter(txt)
-    M.search(split[#split])
-    return
-  end
-  M._toBlade(txt)
-end
+  local txt = line:match(PATTERNS.include) or line:match(PATTERNS.livewire)
+  if not txt then return end
 
-M.route_name = function(node)
-  if vim.bo.filetype ~= "php" then return end
-  local fn = trs.get_name(trs.child(node, "name"))
-  if fn == "name" then
-    local arg = trs.child(node, "arguments")
-    local val = trs.children(arg, "argument")
-    if not val then return end
-    val = val:gsub("'", "")
-    M._toBlade(val)
+  if line:match(PATTERNS.livewire) then
+    M.search(utils.spliter(txt)[#utils.spliter(txt)])
+  else
+    M._toBlade(txt)
   end
 end
 
-M.view = function(node)
-  if vim.bo.filetype ~= "php" then return end
-  local fn = trs.get_name(trs.child(node, "function"))
-  if fn == "view" then
-    local arg = trs.child(node, "arguments")
-    local val = trs.children(arg, "argument")
-    if not val then return end
-    val = val:gsub("'", "")
-    M._toBlade(val)
-  end
-end
-
-M.tag = function(node)
-  if vim.bo.filetype ~= "html" then return end
-
-  local cmp = ts.get_node_text(node, 0, {})
-  if not cmp then return end
-
-  if check_tag(cmp) then
-    vim.notify_once("Native HTML Tag")
-    return
-  end
-
-  local nt = node:next_sibling()
-  local att = trs.get_name(nt)
-
-  if cmp:find(":", 1, true) then
-    local scmp = utils.spliter(cmp, ":")
-    if att:find(".", 1, true) then
-      local split = utils.spliter(att, ".")
-      M.search(split[#split])
-      return
-    end
-    M.search(scmp[#scmp])
-    return
-  end
-
-  local split = utils.spliter(cmp, "-")
-  local search = split[#split]
-
-  if search == "layouts" or search == "layout" then
-    search = split[#split - 1]
-  end
-  if #split > 3 then
-    search = split[#split - 1] .. "-" .. split[#split]
-  end
-
-  local rc = M.rgcSearch(search)
-  if #rc > 1 then
-    vim.schedule(function()
-      pop.popup(rc)
-    end)
-    return
-  end
-
-  if #rc == 0 then
-    search = split[#split - 1] .. "-" .. split[#split]
-    M.search(search)
-    return
-  end
-
-  vim.cmd("e " .. vim.fn.fnameescape(unpack(rc)))
-end
+-- Create directories automatically when saving files
+vim.api.nvim_create_autocmd("BufWritePre", {
+  callback = function(event)
+    if event.match:match('^%w%w+://') then return end
+    local file = vim.uv.fs_realpath(event.match) or event.match
+    vim.fn.mkdir(vim.fn.fnamemodify(file, ':p:h'), 'p')
+  end,
+})
 
 return M
